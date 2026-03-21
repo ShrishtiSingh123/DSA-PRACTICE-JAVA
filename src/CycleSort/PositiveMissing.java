00360922c49c93db4c9933c7bdb0052c6d152f4e
Shrishti <shrisht2723@gmail.com>
2026-03-21 23:14:44 +0530

    Added cyclesort files

---
+++ src/CycleSort/PositiveMissing.java
@@ -0,0 +1,5 @@
+package CycleSort;
+
+public class PositiveMissing {
+
+}
--- src/SelectionSort/Exo1.java
+++ src/SelectionSort/Exo1.java
@@ -4,11 +4,21 @@ import java.util.Arrays;
 
 public class Exo1 {
     public static void main(String[] args) {
-        int arr[] = {3, 1, 5, 4, 2};
-        selectionSort(arr);
+        int arr[] = {4,5,1,2,3};
+       insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
-
+static void insertion (int []arr){
+ for(int i=0;i<arr.length-1;i++){
+   for(int j=i+1;j>0;j--){
+       if(arr[j]<arr[j-1]){
+           swap(arr,j,j-1);
+       }else {
+           break;
+       }
+   }
+ }
+}
     static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int last = arr.length - i - 1;
